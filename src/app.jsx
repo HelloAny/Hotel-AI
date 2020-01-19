d3421b2fa3f2427d0147b2942bbc91897a5e61fc
Tgo <lyq_996ing@163.com>
2020-01-19 23:24:31 +0800

    完成消息发送ui

---
+++ src/app.jsx
@@ -0,0 +1,68 @@
+import Taro, { Component } from '@tarojs/taro'
+import { Provider } from '@tarojs/mobx'
+import Index from './pages/index'
+
+import counterStore from './store/counter'
+
+import './app.css'
+import 'taro-ui/dist/style/index.scss'
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+const store = {
+  counterStore
+}
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/CustomerService/index',
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      color: "#ccc",
+      selectedColor: "#00f",
+      backgroundColor: "#fff",
+      borderStyle: "black",
+      position: "bottom",
+      list: [{
+        pagePath: "pages/index/index",
+        text: "首页"
+      },{
+        pagePath: "pages/CustomerService/index",
+        text: "客服"
+      }]
+    }
+  }
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render () {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
